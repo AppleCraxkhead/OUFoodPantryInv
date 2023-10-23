19b5436d0dcdb0581e09c01ae5486f54362fff6b
AppleCraxkhead <applecraxkhead@gmail.com>
2023-10-23 14:55:54 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,2 +1,11 @@
 // Can yall see this  
 //yes
+import com.sun.net.httpserver.*;
+import java.io.*;
+public class main{
+public static void main(String[]
+args) throws Exception {
+    HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+}
+
+}
